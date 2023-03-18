d67118e33c68740f62cd114992cb9d5b66698c02
anmol-pabla <64691198+anmol-pabla@users.noreply.github.com>
2023-03-18 18:20:02 +0530

    Add files via upload

---
+++ backend/models/buyer.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+// Create schema
+const BuyerSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	number: {
+		type: Number,
+		required: true
+	},
+	wallet: {
+		type: Number,
+		default: 0
+	},
+	favourite_items: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Item"
+	}]
+});
+
+const Buyer = mongoose.model("Buyer", BuyerSchema);
+
+module.exports = Buyer;
---
+++ backend/models/order.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+// Create schema
+const OrderSchema = new mongoose.Schema({
+    buyer_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Buyer",
+        required: true
+    },
+    vendor_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Vendor",
+        required: true
+    },
+    placed_time: {
+        type: Date,
+        default: Date.now
+    },
+    state: {
+        type: String,
+        default: "PLACED"
+    },
+    item_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+        required: true
+    },
+    cost: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+});
+
+const Order = mongoose.model("Order", OrderSchema);
+
+module.exports = Order;

fbc0e8c4a1e2f445ae03db4e85bb03e4e4783bd2
pranabp-bit <58358551+pranabp-bit@users.noreply.github.com>
2023-03-19 12:04:26 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// Create express app
+const app = express();
+
+// Loading enviroment variables
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+// Set up middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// Serve static assets
+app.use('/public/images', express.static(__dirname + '/public/images/'));
+
+// Setup API endpoints
+app.use("/api/buyers", require("./routes/buyer.route"));
+app.use("/api/vendors", require("./routes/vendor.route"));
+app.use("/api/items", require("./routes/item.route"));
+app.use("/api/orders", require("./routes/order.route"));
+
+// Connection to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("MongoDB database connection established successfully!");
+    }
+});
+
+// Start the server
+app.listen(port, function () {
+    console.log(`Server is running on port ${port}!`);
+});
